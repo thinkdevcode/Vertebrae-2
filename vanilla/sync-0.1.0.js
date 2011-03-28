e4657d7b88b7c661958a7a1d584d1296e7bc0c4e
thinkdevcode <gin4lyfe@gmail.com>
2011-03-28 19:47:01 -0400

    Lib/Vanilla versions added. Readme added.

---
+++ vanilla/sync-0.1.0.js
@@ -0,0 +1,106 @@
+﻿/// <reference path="http://damianedwards.com/files/jquery/jquery-1.5-vsdoc.js" />
+
+// Vertebrae 2 Framework
+// Version: 0.1.3, Last updated: 3/23/2011
+//
+// Project Home - http://www.vertebraejs.com
+// Blog         - http://thinkdevcode.wordpress.com
+// GitHub       - https://github.com/thinkdevcode/Vertebrae-2
+// Contact      - ea@pexelu.com [Eugene Alfonso]
+// 
+// See License.txt for full license
+// 
+// Copyright (c) 2011 Eugene Alfonso,
+// Licensed under the MIT license.
+
+// Sync Module
+// Version: 0.1.0, Last updated: 3/23/2011
+
+(function (window) {
+
+    if (!_v) return;
+
+    _v.require(['https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js', 
+				'http://cdn.vertebraejs.com/misc/json2.min.js'], function () {
+
+        /*
+        *   sync() - calling directly will create a data handler
+        *
+        *       options [object] OR [array(objects)] [not optional]
+        *           { 
+        *                       url : the url to the service, example inputs: 'Default.aspx/Method', 'Method' [not optional]
+        *                      name : an alternative name to use for the method [optional]
+        *               defaultName : The default page name in case JS can't find it in navbar (such as index.html, default.aspx, etc)
+        *           }
+        *
+        */
+        _v.sync = _v.sync || (function () {
+            
+            var sync = function (options) {
+
+                if (options instanceof Object) {
+
+                    if (options instanceof Array) jQuery.each(options, function (i, x) { sync(x); });
+
+                    else if (typeof options.url === 'string') {
+
+                        var name = options.name || options.url;
+                        if (!sync[name]) {
+                            sync[name] = function (obj, succ, err, pre, sync) {
+
+                                var currpage = getPage(),
+                                    fnarray = [],
+                                    paramtest = (typeof obj === 'function'),
+                                    synctype = sync || pre;
+
+                                if (paramtest) {
+                                    fnarray[0] = obj || function () { };
+                                    fnarray[1] = succ || function () { };
+                                    fnarray[2] = err || function () { };
+                                }
+                                else {
+                                    fnarray[0] = succ || function () { };
+                                    fnarray[1] = err || function () { };
+                                    fnarray[2] = pre || function () { };
+                                }
+
+                                jQuery.ajax({
+                                    type: 'POST',
+                                    async: synctype || true,
+                                    url: (/.*\..*/i.test(options.url)) ? options.url :              // see if its a full url
+                                            (currpage !== '') ?
+                                                (currpage + '/' + options.url) :                    // try to use pagename/url
+                                                    (typeof options.defaultName === 'string') ?
+                                                        (options.defaultName + '/' + options.url) : // try to use defaultname/url
+                                                            options.url,                            // if worst comes to worse just try the url, haha
+                                    data: (paramtest) ? '{}' : JSON.stringify(obj),
+                                    contentType: 'application/json; charset=utf-8',
+                                    dataType: 'json',
+                                    beforeSend: fnarray[2],
+                                    error: fnarray[1],
+                                    success: function (e) {
+                                        if (typeof e.d === 'string' && /[{:}]/.test(e.d))
+                                            fnarray[0].call(this, JSON.parse(e.d));
+                                        else
+                                            fnarray[0].call(this, e.d);
+                                    }
+                                });
+                            };
+                        }
+                    }
+                } 
+            }
+
+            function getPage() {
+                return (window.location.pathname).substring((window.location.pathname).lastIndexOf('/') + 1);
+            }
+
+            return (sync);
+
+        })();
+
+        return (window._v = _v);
+
+    });
+
+})(window);
---
+++ vanilla/vertebrae-0.1.3.js
@@ -0,0 +1,161 @@
+﻿/// <reference path="http://damianedwards.com/files/jquery/jquery-1.5-vsdoc.js" />
+
+// Vertebrae 2 Framework
+// Version: 0.1.3, Last updated: 3/23/2011
+//
+// Project Home - http://www.vertebraejs.com
+// Blog         - http://thinkdevcode.wordpress.com
+// GitHub       - https://github.com/thinkdevcode/Vertebrae-2
+// Contact      - ea@pexelu.com [Eugene Alfonso]
+// 
+// See License.txt for full license
+// 
+// Copyright (c) 2011 Eugene Alfonso,
+// Licensed under the MIT license.
+
+(function (window) {
+
+    var vertebrae = vertebrae || {},
+
+		loader = {
+			callbacks: [],     //stack to hold callbacks
+			reqscripts: [],	   // array to hold all loaded files
+			argsTotal: 0,      // how many files in total are being inserted
+			argsLoaded: 0,     // how many files completed
+			watched: false	   // determine if tracking function has already been loaded
+		},
+		
+		execute = function() {
+			for (var i = 0, len = loader.callbacks.length; i < len; i += 1) (function () {
+				var d = loader.callbacks.shift(); 
+				d();
+			})();
+		},
+		
+		watch = function () {
+			if (loader.watched === false) {
+				var timer = window.setInterval(function () {
+					if (loader.argsTotal === loader.argsLoaded) {
+						window.clearInterval(timer);
+						loader.watched = false;
+						execute();
+					}
+				}, 5);
+				loader.watched = true;
+			}
+		};
+	
+    vertebrae.require = (function () {
+
+        var require = function (script, callback) {
+            if (arguments.length == 0) return;
+
+			loader.callbacks.unshift(callback); //add callback to beginning of stack
+			
+            if (typeof script === 'string') {
+                loader.argsTotal += 1;
+                load(script);
+            } else if (script instanceof Array) {
+                loader.argsTotal += script.length;
+                for (var i in script)
+                    load(script[i]);
+            }
+			
+			watch();
+					
+            function load(url) {
+                
+                var head = document.head || document.getElementsByTagName('head')[0], // we add tags to <head>
+                    id = 'script' + Math.floor(Math.random() * 1234),   // random id generator
+                    tag = null; // used for building tag to insert
+
+                // check to see if url is loaded already
+                function notLoaded(src, type) { 
+
+                    var nodes,      // element list for checking if file is included in page
+                        property,   // property = 'src' for <script> and 'href' for <link>
+                        nodeArray,  // function to get nodes in array
+                        i = 0,
+                        scrlen = loader.reqscripts.length;
+
+                    // see if file is in global script array
+                    for (; i < scrlen; i += 1) {  
+                        if (loader.reqscripts[i] === src) {
+                            return false;
+                        }
+                    }
+
+                    if (type === 'js') {
+                        nodes = document.getElementsByTagName('script');
+                        property = 'src';
+                    } else if (type === 'css') {
+                        nodes = document.getElementsByTagName('link');
+                        property = 'href';
+                    }
+
+                    nodeArray = (function () {
+                        var arr = [],
+                            count = nodes.length;
+                        for (; count-- > 0; ) {
+                            if (count in nodes) {
+                                arr.push(nodes.count);
+                            }
+                        }
+                        return arr;
+                    });
+
+                    var c = 0,
+                        len = nodeArray.length;
+                    for (; c < len; c += 1) {
+                        if ((property in nodeArray[c]) && nodeArray[c][property] === src) return false;
+                    }
+
+                    return true; // file isnt loaded already
+                }
+
+                // if inserting a js file, create <script> tag
+				if (url.indexOf('.js') > -1 && notLoaded(url, 'js')) {  
+					loader.reqscripts.push(url);
+					tag = document.createElement('script');
+					tag.setAttribute('type', 'text/javascript');
+					tag.src = url;
+					tag.id = id;
+
+					if (tag.onload || tag.onload === null || /Firefox[\/\s](\d+\.\d+)/.test(navigator.userAgent)) {
+						tag.onload = function () {
+							loader.argsLoaded += 1;
+						}
+					} else {
+						tag.onreadystatechange = function () {
+							if (this.readyState === "loaded" || this.readyState === "complete") {
+								loader.argsLoaded += 1;
+							}
+						}
+					}
+                }
+
+                // if inserting a css file, create <link> tag
+                else if (url.indexOf('.css') > -1 && notLoaded(url, 'css')) { 
+					loader.reqscripts.push(url);
+					loader.argsLoaded += 1; // there is no onload event for link tags, so just increment
+					tag = document.createElement('link');
+					tag.rel = 'stylesheet';
+					tag.type = 'text/css';
+					tag.href = url;
+					tag.id = id;
+				}
+				
+                if (tag) {
+					head.appendChild(tag);
+                } else {
+					loader.argsLoaded += 1; // already loaded so increment counter
+				}
+				
+            }
+        }
+        return (require);
+    })();
+
+    return (window._v = vertebrae);
+
+})(window);
